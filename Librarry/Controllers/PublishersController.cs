107f708cac6f510cd70be997003205ccb0babc07
Andrii <andrijprihodko@gmail.com>
2021-08-25 21:57:10 +0300

    add all

---
+++ Librarry/Controllers/PublishersController.cs
@@ -0,0 +1,78 @@
+﻿using Librarry.Data.Services;
+using Librarry.Data.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Librarry.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishersController : ControllerBase
+    {
+        private readonly PublishersService _publisherService;
+
+        public PublishersController(PublishersService publishersService)
+        {
+            _publisherService = publishersService;
+        }
+
+        [HttpGet("get-all-publishers")]
+        public IActionResult GetAllPublishers()
+        {
+            var allPublishers = _publisherService.GetAllPublishers();
+            return Ok(allPublishers);
+        }
+
+        [HttpGet("get-publisher-by-id/{id}")]
+        public IActionResult GetPublisherById(int id)
+        {
+            var _publisher = _publisherService.GetPublisherById(id);
+            if(_publisher != null)
+            {
+                return Ok(_publisher);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("get-publisher-books-with-authors/{id}")]
+        public IActionResult GetPublisherData(int id)
+        {
+            var _publisherData = _publisherService.GetPublisherData(id);
+            if(_publisherData != null)
+            {
+                return Ok(_publisherData);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost("add-publisher")]
+        public IActionResult AddPublisher([FromBody] PublisherVM publisher)
+        {
+            var newPublisher = _publisherService.AddPublisher(publisher);
+            return Created(nameof(AddPublisher), newPublisher);
+        }
+
+        [HttpDelete("delete-publisher-by-id/{id}")]
+        public IActionResult DeletePublisherById(int id)
+        {
+            try
+            {
+                _publisherService.DeletePublisherById(id);
+                return Ok();
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Librarry/Data/Services/AuthorsService.cs
@@ -0,0 +1,57 @@
+﻿using Librarry.Data.Models;
+using Librarry.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Librarry.Data.Services
+{
+    public class AuthorsService
+    {
+        private readonly AppDbContext _context;
+        public AuthorsService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public Author AddAuthor(AuthorVM author)
+        {
+            var _author = new Author()
+            {
+                FullName = author.FullName
+            };
+            _context.Authors.Add(_author);
+            _context.SaveChanges();
+
+            return _author;
+        }
+
+        public AuthorWithBooksVM GetAuthorWithBooks(int authorId)
+        {
+            var _author = _context.Authors.Where(n => n.Id == authorId).Select(n => new AuthorWithBooksVM()
+            {
+                FullName = n.FullName,
+                BookTitles = n.Book_Authors.Select(n => n.Book.Title).ToList()
+            }).FirstOrDefault();
+
+            return _author;
+        }
+
+        public void DeleteAuthorById(int id)
+        {
+            var _author = _context.Authors.FirstOrDefault(n => n.Id == id);
+            if (_author != null)
+            {
+                _context.Authors.Remove(_author);
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new Exception($"The _author with id: {id} not found");
+            }
+        }
+
+    }
+}
